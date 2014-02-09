42f53eb5781dcb440e540220a2d501c642d47477
araynaud <arthur.raynaud@icix.com>
2014-02-08 18:49:00 -0800

    dir template text size change

--- js/mt.ui.js
+++ js/mt.ui.js
@@ -331,7 +331,7 @@ UI.captionClasses = function(mediaFile)
 	var classes= "caption";
 	//if(album.columns<1 && mediaFile.type!="DIR") classes+= "Below";
 	if(album.fadeIn && mediaFile.tnsizes) classes+=" hidden";
-	if(mediaFile.type!="DIR" && ( album.tnIndex == 0 || album.columns>1))
+	if(album.tnIndex == 0 || album.columns>1)
 		classes+=" small";
 
 	return classes;
