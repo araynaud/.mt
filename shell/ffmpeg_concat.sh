6f27ad903ff3e48ce7b8e843590a5085a37b5342
araynaud <arthur@ARMacBookPro.local>
2015-01-26 15:05:55 -0800

    ffmpeg concat shell script

---
+++ shell/ffmpeg_concat.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# directory inputFile outputFilename input files 
+
+echo args: $@
+echo argc: $#
+output=$1
+
+input=""
+shift
+sep=""
+while test ${#} -gt 0
+do
+	echo $1
+	input="$input \"$1\""
+
+	ffmpeg -i "$1" -c copy -an -bsf:v h264_mp4toannexb -f mpegts "$1.ts"
+	filenames=$filenames$sep"$1.ts"
+	sep="|"
+	shift
+done
+
+echo output: $output
+echo input: $input
+echo filenames: $filenames
+
+ffmpeg -i "concat:$filenames" -c copy -bsf:a aac_adtstoasc -movflags faststart $output
+echo input: "$filenames"
+rm *.ts
