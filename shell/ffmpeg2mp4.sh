b5bccd340d405fcc4697c7542a7c1eb99cbf71cd
araynaud <arthur@ARMacBookPro.local>
2015-01-28 11:46:59 -0800

    Same shell script for convert to mp4 / m4v, resize and split

--- shell/ffmpeg2mp4.sh
+++ shell/ffmpeg2mp4.sh
@@ -1,12 +1,32 @@
 #!/bin/bash
 # directory inputFile outputFilename resizeHeight
-echo params: $1 $2 $3 $4
-if [ "$4" = "" ]; then
-	echo "no resize"
-	filter=""
-else
+echo params: $@
+
+oext=mp4
+if [ ${2: -4} = ".mp4" ]; then
+	oext=m4v
+fi
+echo output extension: $oext
+
+if [ "$4" != "" ]; then
 	filter="-vf scale=-1:$4"
-	echo "resize Height: $4"
 fi
 
-ffmpeg -i "$1/$2" $filter -b:v 800k -ab 128k -ac 2 -strict -2 -movflags faststart -y "$1/$3.mp4"
+outputFile=$3
+if [ "$5" != "" ]; then
+	ss="-ss $5"
+	outputFile="${outputFile}_$5"
+fi
+if [ "$6" != "" ]; then
+	to="-to $6"
+	outputFile="${outputFile}_$6"
+fi
+outputFile="$outputFile.$oext"
+
+#ffmpeg_options=-c copy
+ffmpeg_options="$ss $to $filter -b:v 800k -ab 128k -ac 2 -strict -2 -movflags faststart"
+
+ffmpeg -i "$1/$2" $ffmpeg_options -y "$1/$outputFile"
+
+echo ffmpeg -i "$1/$2" $ffmpeg_options -y "$1/$outputFile"
+echo output: $outputFile
