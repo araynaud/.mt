cec0f8797eb375b2d0899d4112e79c4920b106d4
araynaud <arthur.raynaud@icix.com>
2015-05-29 20:26:45 -0700

    dir2mp4 conversion: default dir = .

--- cmd/dir2mp4.bat
+++ cmd/dir2mp4.bat
@@ -1,1 +1,4 @@
-for %%G in (m2t mpg mpeg mts avi mov ogv wmv) do FORFILES /P %1 -m *.%%G /C "cmd /c call ffmpeg2mp4 . @file @fname"
+set inputdir=.
+IF NOT (%4) == () set inputdir=%1
+
+for %%G in (m2t mpg mpeg mts avi mov ogv wmv) do FORFILES /P %inputdir% -m *.%%G /C "cmd /c call ffmpeg2mp4 . @file @fname"
