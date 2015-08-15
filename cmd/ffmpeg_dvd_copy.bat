ebda818afcc67ae0beab6a64811b5125618d2180
Arthur Raynaud <minorart@free.fr>
2015-08-15 14:53:24 -0700

    dvd copy fix

--- cmd/ffmpeg_dvd_copy.bat
+++ cmd/ffmpeg_dvd_copy.bat
@@ -6,4 +6,4 @@ IF NOT (%1) == () set letter=%1
 set title=1
 IF NOT (%2) == () set title=%2
 
-ffmpeg -i "concat:%letter%:\VIDEO_TS\VTS_01_1.VOB|%letter%:\VIDEO_TS\VTS_01_2.VOB" -an -c copy %3
+ffmpeg -i "concat:%letter%:\VIDEO_TS\VTS_0%title%_1.VOB|%letter%:\VIDEO_TS\VTS_0%title%_2.VOB" -an -c copy %3
