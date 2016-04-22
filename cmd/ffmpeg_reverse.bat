57f7f04b773a0402f973d17f27e52338aa938b0c
araynaud <arthur.raynaud@icix.com>
2016-04-22 10:31:59 -0700

    ffmpeg_reverse: specify framerate and delete tmp images

--- cmd/ffmpeg_reverse.bat
+++ cmd/ffmpeg_reverse.bat
@@ -1,6 +1,11 @@
 SET TMP_DIR=.\tmp
+
+set framerate=
+IF NOT (%1) == () set framerate=-r %3
+
 ffmpeg -i %1 -an -qscale 1 -vf yadif %TMP_DIR%\%1.%%07d.jpg 
 rem dir %TMP_DIR% /O-N /B > %TMP_DIR%\list.txt
 call rename_reverse.bat %TMP_DIR% %1
-ffmpeg -i %TMP_DIR%\%1.R%%07d.jpg -r 30 %2
-rem del %TMP_DIR%\%1.R*.jpg
+ffmpeg %framerate% -i %TMP_DIR%\%1.R%%07d.jpg %2
+
+IF (%4) == (delete) del %TMP_DIR%\%1.R*.jpg
