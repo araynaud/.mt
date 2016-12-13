bbf4a7e6466ef87e7fa2c8b5c8e314937e73147f
araynaud <arthur.raynaud@icix.com>
2016-12-12 18:57:43 -0800

    dir2mp3 script

---
+++ cmd/dir2mp3.bat
@@ -0,0 +1,5 @@
+set inputdir=.
+IF NOT (%1) == () set inputdir=%1
+FORFILES /P %inputdir% -m *.mp4 /C "cmd /c call ffmpeg2mp3 . @file @fname"
+
+for %%G in (m2t mpg mpeg mts avi mov ogv wmv vob) do FORFILES /P %inputdir% -m *.%%G /C "cmd /c call ffmpeg2mp3 . @file @fname"
