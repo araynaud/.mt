f94b08da1152e5054e30a335aac3984c3a3519b6
Arthur Raynaud <arthur.raynaud@icix.com>
2019-09-25 20:36:20 -0700

    Update ffmpeg_crop.bat

--- cmd/ffmpeg_crop.bat
+++ cmd/ffmpeg_crop.bat
@@ -1,1 +1,6 @@
-ffmpeg -i %1 -vf crop=%3:%4 -movflags faststart %2.mp4
+SETLOCAL
+set crop=%3:%4
+IF NOT (%5) == () set crop=%crop%:%5
+IF NOT (%6) == () set crop=%crop%:%6
+echo crop %crop%
+ffmpeg -i %1 -vf crop=%crop% -acodec copy -movflags faststart %2.mp4
